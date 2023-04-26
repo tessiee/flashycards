68ee5731defcebb9241d40fb2991c702dcf4b62a
Tessiee <tessa-vivian@hotmail.com>
2023-04-25 23:16:29 -0500

    added functions and json files

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// VARIABLES
+var language = "Spanish";
+var visibleSets;
+function handleClick(event) {
+    var target = event.target;
+    console.log(target.innerHTML);
+}
 // LOG IN BUTTON
 function showLoginWindow() {
     console.log("works!");
@@ -7,41 +14,85 @@ function showRegistrationForm() {
     console.log("works too!");
 }
 // LEFT SIDE BAR
-let visibleSets = [];
-
-function pickCategory() {
-    const pickedCategory = event.target.parentElement.parentElement.parentElement;
-visibleSets = pickedCategory.secondChild;
-console.log(pickedCategory);
-console.log(visibleSets);
-}
-
-function hideSets() {
-    for (let i = 0; i < visibleSets.length; i++) {
-        visibleSets[i].classList.add("invisible");
-    }
-}
-function showSets() {
-    pickCategory();
+function showSets(event) {
+    var pickedCategory = event.target;
+    //visibleSets = pickedCategory.children as HTMLCollection;
     for (var i = 0; i < visibleSets.length; i++) {
         if (visibleSets[i].classList.contains("invisible")) {
             visibleSets[i].classList.remove("invisible");
         }
         else {
-            hideSets();
+            visibleSets[i].classList.add("invisible");
         }
     }
-    console.log(visibleSets);
+}
+function openSet() {
+    console.log("woorks");
+}
+function setLanguage() {
+    var element = EventTarget;
+    console.log(element);
+    console.log(language);
+    //showSetsForLanguage();  NOT DONE
+}
+function showSetsForLanguage() {
+    switch (language) {
+        case "Spanish":
+            var dutchSets1 = document.getElementsByClassName("dutch");
+            for (var i = 0; i < dutchSets1.length; i++) {
+                dutchSets1[i].classList.add("invisible");
+            }
+            var spanishSets1 = document.getElementsByClassName("spanish");
+            for (var i = 0; i < spanishSets1.length; i++) {
+                spanishSets1[i].classList.remove("invisible");
+            }
+            break;
+        case "Dutch":
+            var dutchSet2 = document.getElementsByClassName("dutch");
+            for (var i = 0; i < dutchSet2.length; i++) {
+                dutchSet2[i].classList.remove("invisible");
+            }
+            var spanishSets2 = document.getElementsByClassName("spanish");
+            for (var i = 0; i < spanishSets2.length; i++) {
+                spanishSets2[i].classList.add("invisible");
+            }
+            break;
+        default:
+            language = "Spanish";
+            break;
+    }
 }
 // RIGHT SIDE BAR
 function createNewSet() {
     console.log("works!");
 }
 // CARDS
+function showHint() {
+    var translation = document.getElementById("translation");
+    translation.classList.add("hint");
+}
 function showTranslation() {
-    console.log("works!");
+    var translation = document.getElementById("translation");
+    translation.classList.remove("invisible");
+}
+function hideTranslation() {
+    var translation = document.getElementById("translation");
+    translation.classList.add("invisible");
+}
+function revealTranslation() {
+    showTranslation();
+    showNextButton();
+}
+function showNextButton() {
+    var nextButton = document.getElementById("nextButton");
+    nextButton.classList.remove("invisible");
+}
+function hideNextButton() {
+    var nextButton = document.getElementById("nextButton");
+    nextButton.classList.add("invisible");
 }
 function nextWord() {
-    console.log("works!");
+    hideTranslation();
+    hideNextButton();
 }
 // FOOTER
--- script.ts
+++ script.ts
@@ -1,3 +1,21 @@
+// VARIABLES
+let language: String = "Spanish";
+let visibleSets: any;
+
+// INTERFACES
+interface HTMLEvent extends Event {
+  target: HTMLElement;
+}
+
+type HTMLElementEvent<T extends HTMLElement> = Event & {
+  target: T;
+}
+
+function handleClick(event: HTMLElementEvent<HTMLElement>) {
+  const { target } = event
+  console.log(target.innerHTML);
+}
+
 // LOG IN BUTTON
 function showLoginWindow() {
   console.log("works!");
@@ -6,26 +24,99 @@ function showLoginWindow() {
 function showRegistrationForm() {
   console.log("works too!");
 }
+
 // LEFT SIDE BAR
-let visibleSets: Object[] = [];
 
-function hideSets(){
+function showSets(event: HTMLEvent) {
+  const pickedCategory = event.target;
+  //visibleSets = pickedCategory.children as HTMLCollection;
+  for (let i = 0; i < visibleSets.length; i++) {
+    if (visibleSets[i].classList.contains("invisible")) {
+      visibleSets[i].classList.remove("invisible");
+    } else {
+      visibleSets[i].classList.add("invisible");
+    }
+  }
 }
 
-function showSets() {
-  console.log(visibleSets);
+function openSet() {
+  console.log("woorks");
 }
+
+function setLanguage() {
+  let element = EventTarget;
+  console.log(element);
+  console.log(language);
+  //showSetsForLanguage();  NOT DONE
+}
+
+function showSetsForLanguage() {
+  switch (language) {
+    case "Spanish":
+      let dutchSets1: HTMLCollectionOf<Element> = document.getElementsByClassName("dutch");
+      for (let i = 0; i < dutchSets1.length; i++) {
+      dutchSets1[i].classList.add("invisible");
+      }
+      let spanishSets1: HTMLCollectionOf<Element> = document.getElementsByClassName("spanish");
+      for (let i = 0; i < spanishSets1.length; i++) {
+      spanishSets1[i].classList.remove("invisible");
+      }
+      break;
+    case "Dutch":
+      let dutchSet2: HTMLCollectionOf<Element> = document.getElementsByClassName("dutch");
+      for (let i = 0; i < dutchSet2.length; i++) {
+      dutchSet2[i].classList.remove("invisible");
+      }
+      let spanishSets2: HTMLCollectionOf<Element> = document.getElementsByClassName("spanish");
+      for (let i = 0; i < spanishSets2.length; i++) {
+      spanishSets2[i].classList.add("invisible");
+      }
+      break;
+      default:
+        language = "Spanish"
+        break;
+  }
+}
+
 // RIGHT SIDE BAR
 function createNewSet() {
   console.log("works!");
 }
 // CARDS
+
+function showHint() {
+  let translation: HTMLElement = document.getElementById("translation") as HTMLInputElement;
+  translation.classList.add("hint");
+}
+
 function showTranslation() {
-  console.log("works!");
+  let translation: HTMLElement = document.getElementById("translation") as HTMLInputElement;
+  translation.classList.remove("invisible");
+}
+
+function hideTranslation() {
+  let translation: HTMLElement = document.getElementById("translation") as HTMLInputElement;
+  translation.classList.add("invisible");
+}
+
+function revealTranslation() {
+  showTranslation();
+  showNextButton();
+}
+
+function showNextButton() {
+  let nextButton: HTMLElement = document.getElementById("nextButton") as HTMLInputElement;
+  nextButton.classList.remove("invisible");
+}
+
+function hideNextButton() {
+  let nextButton: HTMLElement = document.getElementById("nextButton") as HTMLInputElement;
+  nextButton.classList.add("invisible");
 }
 
 function nextWord() {
-  console.log("works!");
+  hideTranslation();
+  hideNextButton();
 }
 // FOOTER
 
